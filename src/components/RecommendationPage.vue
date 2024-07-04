31a092c7a50896a933599d6a982ceae4bc240490
cratfisher <93045778+YJX040@users.noreply.github.com>
2024-07-04 08:49:16 +0800

    Update RecommendationPage.vue

--- src/components/RecommendationPage.vue
+++ src/components/RecommendationPage.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="recommendation-page">
       <el-table :data="recommendations">
-        <el-table-column prop="title" label="题目标题" width="180"/>
-        <el-table-column prop="difficulty" label="难度" width="180"/>
-        <el-table-column prop="tags" label="标签" width="180"/>
+        <el-table-column prop="title" label="题目标题" />
+        <el-table-column prop="difficulty" label="难度" "/>
+        <el-table-column prop="tags" label="标签" />
       </el-table>
     </div>
   </template>
